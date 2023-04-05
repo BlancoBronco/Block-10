ea926ad567dd1269fb46e45299ebdaddf9b27f3c
LaMar Fisher <lsfisher503@gmail.com>
2023-04-05 14:44:42 -0700

    debugging form

--- docs/script.js
+++ docs/script.js
@@ -1,26 +1,25 @@
-// Get the form element
-const myform = document.getElementById('my-form');
+<script>
+  // Select the first form
+  const form1 = document.querySelector('form:first-of-type');
 
-// Add an event listener to the form
-myform.addEventListener('submit', function(event) {
+  // Add a submit event listener to the first form
+  form1.addEventListener('submit', function(event) {
+    // Prevent the default form submission behavior
     event.preventDefault();
-    // Prevent the form from submitting
 
-    // Get the form data
-    var formData = new FormData(myform);
+    // Redirect the user to form.html
+    window.location.href = 'form.html';
+  });
 
-    //Make a POST request to the server
-    fetch('submit.php', {
-        method: "POST", 
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        console.log(data); // Log the response from the server
-    })
-    .catch(error => {
-        console.error(error); //Log any errors trhat occur
-    });
+  // Select the second form
+  const form2 = document.querySelector('form:last-of-type');
 
-    // Will send to dummy data page
-});
+  // Add a submit event listener to the second form
+  form2.addEventListener('submit', function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Redirect the user to form.html
+    window.location.href = 'form.html';
+  });
+</script>
